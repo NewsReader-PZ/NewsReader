0e57b2eb8dab17e35be97e493e00bb06f0672d87
jarekjat <jarekjatczak12@gmail.com>
2020-12-08 00:51:50 +0100

    Toolbar in progress, added dependencies, new list_item for more important articles

--- app/src/main/java/com/example/newsreader/ArticleData.kt
+++ app/src/main/java/com/example/newsreader/ArticleData.kt
@@ -4,6 +4,10 @@ import android.icu.text.CaseMap
 
 class ArticleData(){
     var title: String = "Tytuł"
+    var publishingDate = ""
+    var updateDate = ""
+    lateinit var imagesDescription :ArrayList<String>
+    lateinit var imagesAuthors :ArrayList<String>
     var author:String = "Autor"
     var text:String = "texttexttext"
     lateinit var images:ArrayList<String>
--- app/src/main/java/com/example/newsreader/MainActivity.kt
+++ app/src/main/java/com/example/newsreader/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.newsreader
 
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
@@ -20,10 +22,11 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        val appBarConfiguration = AppBarConfiguration(setOf(
+//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+               //findViewById<Toolbar>(R.id.main_toolbar)
+        //setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-    }
 
-}
+    }
+    }
