dcc7b096e3caa674cd6f1410b5b716378d1f4c08
jarekjat <jarekjatczak12@gmail.com>
2020-12-23 22:25:58 +0100

    Deleted unused ArticleFullData class (moved to Repository as CurrentArticle object)

--- app/src/main/java/com/example/newsreader/ui/article/ArticleViewModel.kt
+++ app/src/main/java/com/example/newsreader/ui/article/ArticleViewModel.kt
@@ -1,14 +1,10 @@
 package com.example.newsreader.ui.article
 
 import androidx.lifecycle.ViewModel
-import com.example.newsreader.ArticleData
-import com.example.newsreader.ArticleFullData
 import com.example.newsreader.Repository
 
 class ArticleViewModel(val articleId: String) : ViewModel() {
-//        var article: ArticleFullData?
         init {
             Repository.setCurrentArticle(articleId)
-            //article = Repository.currentArticle.value
         }
 }
